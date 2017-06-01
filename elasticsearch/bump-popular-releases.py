70ee6e7451dbf18a30e2c69f33cb15a2e2df2e1b
James Filtness <jamesfiltness@gmail.com>
2017-06-01 16:40:00 +0100

    Add test data

--- elasticsearch/bump-popular-releases.py
+++ elasticsearch/bump-popular-releases.py
@@ -24,7 +24,20 @@ headers={
 
 releasesBumped = 0
 
-for release in someRelease:
+releaseTest = [
+'03212229-7462-38b7-99e8-c5069209d192' # Reinventing the Steel
+'75a041d3-1fa8-3082-bf4a-ad65416c4624' # The Great Southern Trendkill
+'814e0b20-6884-31c5-9d99-d215f048cfd6' # Vulgar Display of Power
+'a0398241-a842-3593-83b4-a1aa1df23328' # Cowboys From Hell
+'2abca5cc-8985-315f-b948-8a34df27debd' # Power Metal
+'60b55a8c-fe68-3397-91a3-7444cab57d7b' # Far Beyond Driven
+'14e76406-39e6-314b-8b1f-1f93857a4aff' # Metal Magic
+'30f8095a-7ed8-34bf-9355-511224e8d670' # Projects in the Jungle
+'d2814a47-c8fa-39a4-a5dc-9e4c0209cfff' # I Am the Night
+'eafab7b9-4f8f-3083-861c-7d13869ba2ff' # Maiden Voyage
+]
+
+for release in testRelease:
   print "------------------------------------------------------------"
   elasticSearchUrl = 'http://localhost:9200/releases/release/' + release
   elasticSearchRequest = requests.get(elasticSearchUrl, headers=headers)
